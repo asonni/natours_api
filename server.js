10b93e8d436658b115038e6e2ee1c2eeb1c55a92
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-11-14 14:11:31 +0200

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -40,6 +40,20 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR 🔥:', err);
+  });
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
